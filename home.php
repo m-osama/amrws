a4ff3b9f34ece0fb6ea0ebb66aee78e164f4b6cf
Mohammed Osama <mohammedosama@gmail.com>
2015-08-06 22:08:47 +0200

    first upload

---
+++ home.php
@@ -0,0 +1,57 @@
+<html>
+    <!-- test 1 start here -->
+    <div class="container"> 
+      <div class="jumbotron">
+        <h1>Amr|Osama</h1>
+        <h4 class="h3color">&nbsp;&nbsp;&nbsp;<i class="fa fa-android">&nbsp;android developer</i></h4>
+        <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <div align="right">
+          <p><a class="btn btn-primary btn-lg">Learn more</a></p>
+        </div>
+      </div>
+    </div>
+
+    <!-- test 1 end here -->
+    <!-- test 2 start -->
+      <div class="container">
+        <div class="col-md-10 col-md-offset-1">
+          <h3 class="h3color"><i class="fa fa-html5"></i> &nbsp; HTML</h3>
+          <div class="progress ">
+            <div class="progress-bar" style="width: 70%"></div>
+          </div>
+          <h3 class="h3color"><i class="fa fa-css3"></i> &nbsp;css</h3>  
+          <div class="progress ">
+            <div class="progress-bar progress-bar-info" style="width: 50%"></div>
+          </div>
+          <h3 class="h3color"><i class="fa fa-android"></i> &nbsp;Android Development</h3>
+          <div class="progress progress-striped active">
+            <div class="progress-bar progress-bar-success" style="width: 85%"></div>
+          </div>
+          <h3 class="h3color"><i class="fa fa-file-code-o"></i>&nbsp; JS</h3>
+          <div class="progress ">
+            <div class="progress-bar progress-bar-warning" style="width: 40%"></div>
+          </div>
+          <h3 class="h3color"><i class="fa fa-database"></i>&nbsp; SQL/SQLite</h3>
+          <div class="progress ">
+            <div class="progress-bar progress-bar" style="width: 60%"></div>
+          </div>
+        </div>
+      </div>
+    <!-- test 2 end  -->
+<br>
+    <!-- test 3 start -->
+
+<div class="container">
+      <blockquote>
+        <p>If debugging is the process of removing software bugs, then programming must be the process of <br>putting them in.</p>
+          <small>Edsger Dijkstra in <cite title="Source Title">java code geeks</cite></small>
+      </blockquote>
+      </div>
+<br>
+    <!-- test 3 end  -->
+    
+
+
+   
+
+</html>
---
+++ views/header.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>Amr|Osama</title>
+
+    <!-- Mobile support -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- Material Design for Bootstrap -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="dist/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="dist/css/font-awesome.min.css">
+    <!-- <link href="dist/css/bootswatch.min.css" rel="stylesheet"> -->
+    <!-- <link href="dist/css/roboto.min.css" rel="stylesheet"> -->
+    <!-- <link href="dist/css/material-fullpalette.min.css" rel="stylesheet"> -->
+    <!-- <link href="dist/css/ripples.min.css" rel="stylesheet"> -->
+    <script type="text/javascript" async="" src="https://ssl.google-analytics.com/ga.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Dropdown.js -->
+    <link href="//cdn.rawgit.com/FezVrasta/dropdown.js/master/jquery.dropdown.css" rel="stylesheet">
+    <link rel="shortcut icon" href="http://mosama.me/amr/favicon.ico" type="image/x-icon" />
+
+  </head>
+  <body>
+  <!-- inner style start here-->
+    <style nonce="z2c/KUU1H7aIEDkZL8YYnN1Z/i4">
+        .color1 {
+          color: #ffffff;
+          background-color: #FF8A80;
+        }
+        .text1 {
+          color: #ffffff;
+        }
+        .text2 {
+          color: #7f8c8d;
+        }
+        .h3color {
+          color :#999999;
+        }
+        .color2 {
+          color: #ffffff;
+          background-color: #1abc9c;
+        }
+        .color3 {
+          color: #424242;
+          background-color: #B39DDB;
+        }
+        .bgcolor1 {
+          background-color: #4CAF50;
+        }
+        .colorfooter {
+          color: #2196F3;
+          background-color: #F9F9F9;
+          position: relative;
+          margin-bottom: 0px;
+          margin-top: 0px; 
+          bottom: 0px;
+          margin: 0px;
+          border: 0;
+          width: 100%;
+        }
+        .imgleft {
+          left: 0%;
+        }
+        .text3 {
+              color: #424242;
+              font-size: 20px;
+              font-weight: 400;
+              line-height: 28px;
+            }
+
+      </style>
+      <!-- inner style end here -->
+
+    <!-- nav start here -->
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+  <div class="col-md-10 col-md-offset-1">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="http://mosama.me/amr/">Amr|Osama</a>
+    </div>
+    
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+      <ul class="nav navbar-nav">
+        <li><a href="androidapps.php">android ApPs<span class="sr-only">(current)</span></a></li>
+        <li><a href="colors.php">Colors</a></li>
+        <li><a href="contactus.php">Contact me</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+
+      <form class="navbar-form navbar-right" role="search">
+        <div class="form-group ">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+      </form>
+      
+    </div>
+    </div>
+  </div>
+</nav>
+    <!-- nav end here -->
